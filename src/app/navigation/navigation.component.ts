0f29c8191bcb1af096beeaa714476bd17691c7e6
David Lino <david@cucafresca.com.br>
2024-02-19 14:31:10 -0300

    header do profile o usuario e o saldo agora aparecem em todos componentes

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -21,4 +21,34 @@ export class NavigationComponent {
         this.sidebarState = state
       })
   }
+
+
+
+  
+  public eyeCondition: boolean = true;
+
+  user: string = 'David Eduardo Lino'
+  saldo: string = ' 300 $'
+  bSaldo = this.saldo
+
+  eye() {
+    setTimeout(() => {
+
+
+      this.eyeCondition = !this.eyeCondition;
+      if (!this.eyeCondition)
+
+        this.saldo = ''
+
+          ;
+
+      else
+
+        this.saldo = this.bSaldo
+
+          ;
+
+
+    }, 400);
+  }
 }
