a5019ff5ab82dbf611ac559946864d3c3783404f
David Lino <david@cucafresca.com.br>
2024-02-19 14:33:17 -0300

    declarando as funcoes do componente pai e do filho

--- src/app/pages/menu/pix/newpix/newpix.component.ts
+++ src/app/pages/menu/pix/newpix/newpix.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PixComponent } from '../pix.component';
 
 @Component({
   selector: 'app-newpix',
-  standalone: true,
-  imports: [],
+
   templateUrl: './newpix.component.html',
-  styleUrl: './newpix.component.scss'
+  styleUrl: './newpix.component.scss',
 })
 export class NewpixComponent {
 
+  @Output() public fecharPix: EventEmitter<boolean> = new EventEmitter(); //quando é um componente filho
+
+@Input() public tipoPix: string ;//quando é um componente pai
+
+
+  
 }
