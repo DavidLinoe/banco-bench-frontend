ba0ad33b94f9bb47862f0edd3abd666633dbbb8f
David Lino <david@cucafresca.com.br>
2024-02-29 16:16:25 -0300

    mudanca no tratamento do usuario e saldo agora nao é um valor fixo, vem do banco

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,53 +1,38 @@
 import { Component } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
-
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
+  public sidebarState = '';
+  constructor(
+    private sidebarService: NbSidebarService,
+    public userService: UserService
+  ) {}
 
-  public sidebarState = '' 
-  constructor(private sidebarService: NbSidebarService) {
-  }
-
- 
   toggleCompact() {
     this.sidebarService.toggle(true, 'left');
-    this.sidebarService.getSidebarState('left')
-      .subscribe(state => {
-        console.log(state)
-        this.sidebarState = state
-      })
+    this.sidebarService.getSidebarState('left').subscribe((state) => {
+      console.log(state);
+      this.sidebarState = state;
+    });
   }
 
+  r="R$ "
+  saldo = this.r +this.userService.usuario.getValue().saldo_cliente; 
+  bSaldo = "***";
 
-
-  
   public eyeCondition: boolean = true;
-
-  user: string = 'Luis Henrique Martins Dos Santos'
-  saldo: string = ' '
-  bSaldo = this.saldo
 
   eye() {
     setTimeout(() => {
 
-
       this.eyeCondition = !this.eyeCondition;
-      if (!this.eyeCondition)
-
-        this.saldo = '156800.75 $'
-
-          ;
-
-      else
-
-        this.saldo = this.bSaldo
-
-          ;
-
+      if (this.eyeCondition) this.saldo = this.r + this.userService.usuario.getValue().saldo_cliente ;
+      else this.saldo = this.bSaldo;
 
     }, 400);
   }
