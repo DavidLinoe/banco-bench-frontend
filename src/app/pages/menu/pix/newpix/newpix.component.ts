2632ca717fe5204086cb732cac09e28cb15d4074
David Lino <david@cucafresca.com.br>
2024-02-29 17:01:22 -0300

    form group do pagamento pix

--- src/app/pages/menu/pix/newpix/newpix.component.ts
+++ src/app/pages/menu/pix/newpix/newpix.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output,} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PixComponent } from '../pix.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -13,60 +13,38 @@ export class NewpixComponent {
 
   public payForm: FormGroup;
 
-
-  saldo:string = "30"
-  toPay:string = ""
-
+  toPay: string = '';
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
   ngOnInit(): void {
     this.payForm = this.formBuilder.group({
-      amount: ['', [Validators.min(0), Validators.required]],
-
+      amount: [, [Validators.min(0), Validators.required]],
     });
-    
   }
 
 
 
   @Output() public fecharPix: EventEmitter<boolean> = new EventEmitter(); //quando é um componente filho
 
-@Input() public tipoPix: string ;//quando é um componente pai
+  @Input() public tipoPix: string; //quando é um componente pai
 
+  public qrConditionView: boolean = true;
 
-public qrConditionView: boolean = true;
+  public xInput = 'input[input-pix="x"]';
 
+  public chaveAleatoria: string = '';
 
+  qrView() {
+    console.log(this.chaveAleatoria);
 
+    if (this.chaveAleatoria === '') {
+      alert('Todos os campos devem estar preenchidos!');
+    } else if (this.chaveAleatoria === 'chaveteste') {
+      this.qrConditionView = !this.qrConditionView;
 
-
-public xInput = 'input[input-pix="x"]';
-
-public chaveAleatoria:string = "";
-
-qrView(){
-
- console.log(this.chaveAleatoria);
-
-  if (this.chaveAleatoria === ""){
-    alert("Todos os campos devem estar preenchidos!")
-  
-}
-else if(this.chaveAleatoria === "chaveteste"){
-
-  this.qrConditionView = !this.qrConditionView;
-
-  
-  // window.open('../../../../../assets/images/qrcode-pix.png')
-
-}
-else{
-
-  alert("Chave Invalida")
-
-}
-   
-
-}
-
+      // window.open('../../../../../assets/images/qrcode-pix.png')
+    } else {
+      alert('Chave Invalida');
+    }
+  }
 }
