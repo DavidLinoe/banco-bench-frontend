43f004196ecf05c9ff450705a3aab49c00c849f1
David Lino <david@cucafresca.com.br>
2024-03-01 17:03:36 -0300

    armazenando no localStoager o cliente atual

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
 import { UserService } from '../services/user.service';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   public sidebarState = '';
   constructor(
     private sidebarService: NbSidebarService,
-    public userService: UserService
+    public userService: UserService,
+    private http: HttpClient
   ) {}
 
   toggleCompact() {
@@ -20,20 +23,35 @@ export class NavigationComponent {
       this.sidebarState = state;
     });
   }
+  
+  ngOnInit(): void {
+    const id_cliente= localStorage.getItem("id_cliente")
+    this.enviarDadosUser({id_cliente});
+  }
+  enviarDadosUser(res: any) {
+    this.http.post('http://localhost:3000/user', res).subscribe({
+      next: (res: any) => {
+        console.log('dad', res);
+        this.userService.usuario.next(res.user); //envia a res para o service !
 
-  r="R$ "
-  saldo = this.r +this.userService.usuario.getValue().saldo_cliente; 
-  bSaldo = "***";
+      },
+      error: (err: any) => {
+        console.log('erro');
+      },
+    });
+  }
+  r = 'R$ ';
+  saldo = "***";
+  bSaldo = '***';
 
   public eyeCondition: boolean = true;
 
   eye() {
     setTimeout(() => {
-
       this.eyeCondition = !this.eyeCondition;
-      if (this.eyeCondition) this.saldo = this.r + this.userService.usuario.getValue().saldo_cliente ;
-      else this.saldo = this.bSaldo;
-
+      if (this.eyeCondition)
+      this.saldo = this.bSaldo;
+      else  this.saldo = this.r + this.userService.usuario.getValue().saldo_cliente;
     }, 400);
   }
 }
