ba2cf48e69ebf5e596b5b4c17a96a3d1368c35bf
David Lino <david@cucafresca.com.br>
2024-03-04 16:47:26 -0300

    criando o modal de conferir os dados do pix

---
+++ src/app/pages/menu/pix/pix-modal-conferir-dados/pix-modal-conferir-dados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pix-modal-conferir-dados',
+  standalone: true,
+  imports: [],
+  templateUrl: './pix-modal-conferir-dados.component.html',
+  styleUrl: './pix-modal-conferir-dados.component.scss'
+})
+export class PixModalConferirDadosComponent {
+
+}
