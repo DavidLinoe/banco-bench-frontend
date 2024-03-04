867ec443ae57a45cea720daf50d69b5e43fa869f
David Lino <david@cucafresca.com.br>
2024-03-04 16:47:42 -0300

    criando o modal de comprovante de pagamento

---
+++ src/app/pages/menu/pix/pix-modal-comprovante/pix-modal-comprovante.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pix-modal-comprovante',
+  standalone: true,
+  imports: [],
+  templateUrl: './pix-modal-comprovante.component.html',
+  styleUrl: './pix-modal-comprovante.component.scss'
+})
+export class PixModalComprovanteComponent {
+
+}
