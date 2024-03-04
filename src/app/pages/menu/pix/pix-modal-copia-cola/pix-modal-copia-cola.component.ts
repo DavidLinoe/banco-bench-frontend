5b8f7b1988907ac433b4e146d48b363ad711fb92
David Lino <david@cucafresca.com.br>
2024-03-04 16:47:05 -0300

    criando o modal de pagar com copia e cola

---
+++ src/app/pages/menu/pix/pix-modal-copia-cola/pix-modal-copia-cola.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-pix-modal-copia-cola',
+  templateUrl: './pix-modal-copia-cola.component.html',
+  styleUrl: './pix-modal-copia-cola.component.scss',
+})
+export class PixModalCopiaColaComponent {
+  public payForm: FormGroup;
+
+  public qrConditionView: boolean = true;
+  public chaveAleatoria: string = '';
+
+  qrView() {
+    if (this.chaveAleatoria === '') {
+      alert('Todos os campos devem estar preenchidos!');
+    } else if (this.chaveAleatoria === 'chaveteste') {
+      this.qrConditionView = !this.qrConditionView;
+
+      // window.open('../../../../../assets/images/qrcode-pix.png')
+    } else {
+      alert('Chave Invalida');
+    }
+  }
+
+  fecharModal() {}
+}
