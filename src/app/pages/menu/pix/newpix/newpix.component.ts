f3494b604db7c877f5e0acb8dfe93d5590cb6d91
David Lino <david@cucafresca.com.br>
2024-03-04 16:50:13 -0300

    newpix agora esta separado por componentes modais

--- src/app/pages/menu/pix/newpix/newpix.component.ts
+++ src/app/pages/menu/pix/newpix/newpix.component.ts
@@ -1,7 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { PixComponent } from '../pix.component';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-newpix',
@@ -11,18 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NewpixComponent {
 
-  public payForm: FormGroup;
 
-  toPay: string = '';
-
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
-  ngOnInit(): void {
-    this.payForm = this.formBuilder.group({
-      amount: [, [Validators.min(0), Validators.required]],
-      
-    });
-    
-  }
   mudarChave(){
 
     var chave = document.getElementById('choose');
@@ -32,29 +18,29 @@ export class NewpixComponent {
   }
 
 
-   
-
   @Output() public fecharPix: EventEmitter<boolean> = new EventEmitter(); //quando é um componente filho
 
   @Input() public tipoPix: string; //quando é um componente pai
 
-  public qrConditionView: boolean = true;
 
-  public xInput = 'input[input-pix="x"]';
 
-  public chaveAleatoria: string = '';
 
-  qrView() {
-    console.log(this.chaveAleatoria);
 
-    if (this.chaveAleatoria === '') {
-      alert('Todos os campos devem estar preenchidos!');
-    } else if (this.chaveAleatoria === 'chaveteste') {
-      this.qrConditionView = !this.qrConditionView;
+  tipoIf(){
 
-      // window.open('../../../../../assets/images/qrcode-pix.png')
-    } else {
-      alert('Chave Invalida');
+    
+    this.tipoPix = "pagar"
     }
-  }
+
+
+  pixConfirm(){
+
+    
+    this.tipoPix = "copia e cola"
+    }
+
+
+
+
+
 }
