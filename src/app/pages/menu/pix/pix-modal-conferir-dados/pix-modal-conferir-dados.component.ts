11266c01c672aa7fcac910d6b39cdbcea9662a46
David Lino <david@cucafresca.com.br>
2024-03-05 16:13:16 -0300

    tirando standalone

--- src/app/pages/menu/pix/pix-modal-conferir-dados/pix-modal-conferir-dados.component.ts
+++ src/app/pages/menu/pix/pix-modal-conferir-dados/pix-modal-conferir-dados.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pix-modal-conferir-dados',
-  standalone: true,
-  imports: [],
   templateUrl: './pix-modal-conferir-dados.component.html',
   styleUrl: './pix-modal-conferir-dados.component.scss'
 })
