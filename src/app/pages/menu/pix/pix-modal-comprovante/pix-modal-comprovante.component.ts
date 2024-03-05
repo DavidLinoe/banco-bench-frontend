d04f08a6f5bd2e789ce4d25f4d6b7c1cad3ec543
David Lino <david@cucafresca.com.br>
2024-03-05 16:35:40 -0300

    injetando o user service no modal de comprovante

--- src/app/pages/menu/pix/pix-modal-comprovante/pix-modal-comprovante.component.ts
+++ src/app/pages/menu/pix/pix-modal-comprovante/pix-modal-comprovante.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service';
 
 @Component({
   selector: 'app-pix-modal-comprovante',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './pix-modal-comprovante.component.html',
   styleUrl: './pix-modal-comprovante.component.scss'
 })
-export class PixModalComprovanteComponent {
+export class PixModalComprovanteComponent  {
+constructor(
+  public userService: UserService
+  ){}
+
+
+
+
+
+
+
 
 }
+
