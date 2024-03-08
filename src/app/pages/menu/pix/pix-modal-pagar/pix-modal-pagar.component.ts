e616881989296b895a54b0c6f65db1be20934eaa
David Lino <david@cucafresca.com.br>
2024-03-08 16:53:17 -0300

    novas funcoes para verificar a chave pix

--- src/app/pages/menu/pix/pix-modal-pagar/pix-modal-pagar.component.ts
+++ src/app/pages/menu/pix/pix-modal-pagar/pix-modal-pagar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { PixService } from '../../../../services/pix.service';
 
 @Component({
   selector: 'app-pix-modal-pagar',
@@ -7,15 +9,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './pix-modal-pagar.component.scss',
 })
 export class PixModalPagarComponent {
-  public state: string = "pagar";
+  public state: string = 'pagar';
   public payForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    public pixService: PixService
+  ) {}
   ngOnInit(): void {
     this.payForm = this.formBuilder.group({
-      amount: [0, [Validators.min(0), Validators.required]],
+      amount: [0, [Validators.required]],
+      chave: [],
+      chaveAleatoria: [],
     });
   }
+
   comprovante() {
     this.state = 'comprovante';
   }
@@ -24,5 +33,43 @@ export class PixModalPagarComponent {
   }
   confirm() {
     this.state = 'confirm';
+  }
+
+  waitChange() {
+    if (localStorage.getItem('stateChange') == 'true') {
+      console.log('Log no waitChange pix modal Router Funciona');
+      this.chaveAleatoria();
+    } else {
+      console.log('Log no waitChange pix modal Router NAO FUNCIONAAAAAAA');
+    }
+    console.log('Aguardando Ser True');
+  }
+  chaveAleatoria() {
+    localStorage.setItem('chaveAleatoria', this.payForm.value.chaveAleatoria);
+    console.log(
+      ' log da chave digitada ',
+      localStorage.getItem('chaveAleatoria')
+    );
+
+    this.verificarChave();
+  }
+
+  verificarChave() {
+    this.http
+      .post('http://localhost:3000/verificar/chave', {
+        dadosChave: this.payForm.value.chaveAleatoria,
+      })
+      .subscribe({
+        next: (res: any) => {
+          localStorage.setItem('chavePix', res.telefone.toString());
+          // this.pixService.chaveExistente.next(res.telefone.toString()); //envia a res para o Pix service !
+
+          console.log('Resposta do Next  ', res.telefone.toString());
+        },
+        error: (err: any) => {
+          console.log('erro');
+          localStorage.removeItem('chavePix');
+        },
+      });
   }
 }
