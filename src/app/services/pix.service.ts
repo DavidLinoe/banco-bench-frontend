4b8879e1b2041108e4b701d0f84dc74d733bbe97
David Lino <david@cucafresca.com.br>
2024-03-08 16:55:20 -0300

    novo service para o pix

---
+++ src/app/services/pix.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+interface chave {
+  telefone: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PixService {
+  constructor() { 
+
+    this.chaveExistente.subscribe((value) => console.log("log no service do Pix ",{ value }));
+
+  }
+
+  
+public chaveExistente: BehaviorSubject<chave> = new BehaviorSubject<chave>({
+
+
+  telefone: '',
+
+
+})
+}
+
