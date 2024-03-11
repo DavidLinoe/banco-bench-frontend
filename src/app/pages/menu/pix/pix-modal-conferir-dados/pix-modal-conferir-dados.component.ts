07653915eb991834c9d8383cb75906633d2ec266
David Lino <david@cucafresca.com.br>
2024-03-11 17:00:37 -0300

    input de dados de outro componente

--- src/app/pages/menu/pix/pix-modal-conferir-dados/pix-modal-conferir-dados.component.ts
+++ src/app/pages/menu/pix/pix-modal-conferir-dados/pix-modal-conferir-dados.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, ViewChild } from '@angular/core';
+import { PixModalPagarComponent } from '../pix-modal-pagar/pix-modal-pagar.component';
 
 @Component({
   selector: 'app-pix-modal-conferir-dados',
   templateUrl: './pix-modal-conferir-dados.component.html',
-  styleUrl: './pix-modal-conferir-dados.component.scss'
+  styleUrl: './pix-modal-conferir-dados.component.scss',
 })
 export class PixModalConferirDadosComponent {
+  @Input() public conferirValor: string; //quando é um componente pai
 
 }
