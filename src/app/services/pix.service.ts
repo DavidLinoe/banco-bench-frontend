11e2e3c02f7a92a9d16398834c47f231828cfb7c
David Lino <david@cucafresca.com.br>
2024-03-15 17:08:13 -0300

    pix modal

--- src/app/services/pix.service.ts
+++ src/app/services/pix.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-
 interface chave {
   telefone: string;
+  email: string;
+  cpf: string;
+
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PixService {
-  constructor() { 
-
-    this.chaveExistente.subscribe((value) => console.log("log no service do Pix ",{ value }));
-
+  constructor() {
+    this.chaveExistente.subscribe((value) =>
+      console.log('log no service do Pix ', { value })
+    );
   }
 
-  
-public chaveExistente: BehaviorSubject<chave> = new BehaviorSubject<chave>({
+  public chaveExistente: BehaviorSubject<chave> = new BehaviorSubject<chave>({
+    telefone: '',
+    email: '',
+    cpf: '',
 
-
-  telefone: '',
-
-
-})
+  });
 }
-
