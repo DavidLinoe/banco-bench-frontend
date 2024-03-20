0e267b559e8ef5a7221b346b8423919f1b83cb47
David Lino <david@cucafresca.com.br>
2024-03-20 15:30:52 -0300

    implementacao do download pdf

--- src/app/pages/menu/pix/pix-modal-comprovante/pix-modal-comprovante.component.ts
+++ src/app/pages/menu/pix/pix-modal-comprovante/pix-modal-comprovante.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../../../services/user.service';
 import { PixService } from '../../../../services/pix.service';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-pix-modal-comprovante',
@@ -9,16 +10,37 @@ import { PixService } from '../../../../services/pix.service';
   styleUrl: './pix-modal-comprovante.component.scss',
 })
 export class PixModalComprovanteComponent {
-  constructor(
-    public userService: UserService,
-    public pixService: PixService,
+  @ViewChild('content', { static: false }) content!: ElementRef;
 
-    ) {}
+  constructor(public userService: UserService, public pixService: PixService) {}
 
   nome = this.pixService.chaveExistente.getValue().nome_cliente;
   valor = localStorage.getItem('valorAmount');
 
   cpf = '21432535325';
-  chave = this.pixService.chaveExistente.getValue().telefone || this.pixService.chaveExistente.getValue().email || this.pixService.chaveExistente.getValue().cpf;
+  chave =
+    this.pixService.chaveExistente.getValue().telefone ||
+    this.pixService.chaveExistente.getValue().email ||
+    this.pixService.chaveExistente.getValue().cpf;
   agencia = '24-40/01';
+
+  pdfName: any; // Propriedade para armazenar o nome do PDF
+
+  // downloadPDF() {
+  //   const doc = new jsPDF();
+  //   doc.text('Hello World', 10, 10);
+
+  //   doc.save(this.pdfName + '.pdf'); // Usar o nome do PDF definido pelo usuário
+  // }
+  downloadPDF() {
+    this.pdfName =
+      'Comprovante ' + this.nome + ' ' + localStorage.getItem('last-operation') || null;
+
+    const pdf = new jsPDF('l', 'px', 'a4');
+    pdf.html(this.content.nativeElement, {
+      callback: (pdf) => {
+        pdf.save(this.pdfName + '.pdf'); // Usar o nome do PDF definido pelo usuário
+      },
+    });
+  }
 }
